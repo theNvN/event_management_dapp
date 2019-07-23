0f5a412ceeb2663ff258a4403ad405ca0c1c8e61
Naveen <thenvnsahu@gmail.com>
2019-07-24 01:21:46 +0530

    deployed contract to rinkeby testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 
+// var HDWalletProvider = require('truffle-hdwallet-provider');
+// const infuraUrl = "https://rinkeby.infura.io/v3/<PROJECT-ID>";
+// const mnemonic = "";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,6 +13,13 @@ module.exports = {
       host: "127.0.0.1",
       port: "8545",
       network_id: "*"
-    }
+    },
+
+    // rinkeby: {
+    //   provider: () => new HDWalletProvider(mnemonic, infuraUrl),
+    //   network_id: 4,
+    //   gas: 5500000
+    // }
+
   }
 };
